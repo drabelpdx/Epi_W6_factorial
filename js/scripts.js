b84cf844ff04277fb080597d28226cf1d67a26e7
Steve & David Abel <student@epicodus.com>
2015-05-26 16:27:33 -0700

    adds validation for non numbers.

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
 var factorial = function(number) {
-  var total_value = 1;
-  if (isNaN(number)) {
-    return 'is not a valid input.';
-  }
+
   if (number === 0) {
     return 1;
   }
   return (number * factorial(number-1));
 };
+
+$(document).ready(function() {
+  $("form#factorial").submit(function(event) {
+    var number = parseInt($("input#number").val());
+    var result = 0
+    if (isNaN(number)) {
+      result = "not a valid input";
+      number = "Your entry";
+    } else {
+      var result = factorial(number);
+    }
+
+    $(".result").text(result);
+    $(".number").text(number);
+
+    $("#result").show();
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,18 +1,15 @@
 describe('factorial', function() {
-  it ("returns 1 if 0 is input", function() {
-    expect(factorial(9)).to.equal(362880);
+
+  it ("returns 1 if 0! is input", function() {
+    expect(factorial(0)).to.equal(1);
   });
 
-  // it ("", function() {
-  //   expect( ).to.equal( );
-  // });
-  //
-  // it ("", function() {
-  //   expect( ])).to.equal( );
-  // });
-  //
-  // it ("", function() {
-  //   expect( )).to.equal( );
-  // });
+  it ("returns 24 if 4! is input", function() {
+    expect(factorial(4)).to.equal(24);
+  });
+
+  it ("returns 362880 if 9! is input", function() {
+    expect(factorial(9)).to.equal(362880);
+  });
 
 });
